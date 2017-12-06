72ae7a4e2e63f417182af0f947007053a1cd6971
Johan Sylvan <gtc124@gmail.com>
2017-12-06 10:50:22 +0100

    Functioning card mechanics, place randomly and restart

---
+++ static/js/main.js
@@ -0,0 +1,87 @@
+window.onload = () => {
+    document.querySelector('.restart').addEventListener('click', (event) => {
+        event.preventDefault();
+        newGame(document.querySelector('#num-cards').value);
+    })
+}
+
+const colors = ['red', 'green', 'blue', 'orange', 'magenta', 'yellow', 'slime', 'greyish', 'darkness', 'lightness'];
+
+const cardLogic = (card) => {
+    let flippedCards = document.querySelectorAll('.flip');
+    flippedCards.forEach((card) => {
+        setTimeout(() => { card.classList.remove('flip') }, 2000);
+    });
+}
+
+const shuffle = (cards)=>{
+    cards.forEach((element, i) => {
+        setTimeout(() => {
+            element.classList.add("shuffle")
+        }, 100 * (i-1));
+        setTimeout(() => {
+            element.classList.remove("shuffle")
+        }, (100 * cards.length)+500);
+    });
+}
+
+const positionCards = (cards) => {
+    cards.forEach(card => {
+        card.style.left = Math.floor((Math.random() * (window.innerWidth - 260))+100)+"px";
+        card.style.top = Math.floor((Math.random() * (window.innerHeight - 340)))+"px";
+    });
+}
+
+function findAncestor(el, cls) {
+    while ((el = el.parentElement) && !el.classList.contains(cls));
+    return el;
+}
+
+const createCard = () => {
+    let card = document.createElement('div');
+    card.classList.add('flip-container');
+
+    let flipper = document.createElement('div');
+    flipper.classList.add('flipper');
+
+    let front = document.createElement('div');
+    front.classList.add('front');
+    flipper.appendChild(front);
+
+    let back = document.createElement('div');
+    back.classList.add('back');
+    flipper.appendChild(back);
+
+    card.appendChild(flipper);
+    card.addEventListener('click', (event) => {
+        findAncestor(event.target, "flip-container").classList.toggle('flip');
+        cardLogic(event.target);
+    })
+
+    return card;
+
+}
+
+const randCards = (cards)=>{
+    for (let index = 0; index < cards.length; index+=2) {
+        cards[index].querySelector('.back').style.backgroundColor = `var(--${colors[Math.floor(index/2)]})`;
+        cards[index+1].querySelector('.back').style.backgroundColor = `var(--${colors[Math.floor(index/2)]})`;
+    }
+}
+
+const newGame = (cards) => {
+    document.querySelectorAll(".flip-container").forEach((elmt) => {
+        elmt.remove();
+    });
+    for (let ind = 0; ind < cards; ind++) {
+        document.querySelector('.board').appendChild(createCard());
+    }
+    this.cards = document.querySelectorAll('.flip-container');
+    randCards(this.cards);
+    setTimeout(() => {
+        shuffle(this.cards);
+        setTimeout(() => {
+            positionCards(this.cards);
+        }, 2000);
+    }, 1000);
+}
